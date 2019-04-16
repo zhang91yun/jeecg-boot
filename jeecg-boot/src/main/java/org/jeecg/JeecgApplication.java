c1a4804b19eea7b909e975611fb6be9f051af505
zhang91yun@126.com <zhangy@fsshipping.com>
2019-04-16 15:19:15 +0800

    local dev

--- jeecg-boot/src/main/java/org/jeecg/JeecgApplication.java
+++ jeecg-boot/src/main/java/org/jeecg/JeecgApplication.java
@@ -2,12 +2,19 @@ package org.jeecg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class JeecgApplication {
+public class JeecgApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JeecgApplication.class);
+    }
 
     public static void main(String[] args) {
     	System.setProperty("spring.devtools.restart.enabled", "false");
